077c48d2b52f61351582b63b1f56a935de1261f8
Norbert Czarnota <norbertczarnota@yahoo.co.uk>
2025-08-21 13:57:56 +0100

    Skeleton

---
+++ main.py
@@ -0,0 +1,27 @@
+#variables
+
+#grid util
+
+#interpolations and MAC operators
+
+#convective term? leave untill the end
+
+#poisson solver
+
+#forcing util
+
+#diagnostics
+
+#initial conditions
+
+#blowup detectors
+
+#I/O helpers
+
+#time stepper & main
+
+#batch sweeper
+
+#parser
+
+#entrypoint
